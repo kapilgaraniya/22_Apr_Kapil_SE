f373801fc90f8a0f7a035b0279205c5c62934c34
Kapil Garaniya <167281036+kapilgaraniya@users.noreply.github.com>
2024-05-27 08:26:58 +0530

    Add files via upload

---
+++ Practice/Question.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main()
+{
+	char a,b,c,d,ans;
+	
+	printf("(Que.1) What Is India's Big State By Aria ?\n");
+	printf(" A. Gujrat\n");
+	printf(" B. Maharashtra\n");
+	printf(" C. Rajasthan\n");
+	printf(" D. Panjab\n");
+	printf("Select option Only(a,b,c,d):%c",ans);
+	scanf("%c",&ans);
+	
+	if(ans=='c')
+	{
+		printf("WOW.. Right Answer : C. Rajasthan");
+	}
+	
+	else if(ans == 'a')
+	{
+		printf("You Are Wrong The Right Answer Is : C. Rajasthan");
+	}
+	
+	else if(ans == 'b')
+	{
+		printf("You Are Wrong The Right Answer Is : C. Rajasthan");
+	}
+	
+	else if(ans == 'd')
+	{
+		printf("You Are Wrong The Right Answer Is : C. Rajasthan");
+	}
+	
+	else
+	{
+		printf("Plz... Select A, b, C & D Only !");
+	}
+}
