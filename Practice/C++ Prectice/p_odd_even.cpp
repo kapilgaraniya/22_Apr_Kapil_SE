6b96ca5a42e1d8d4bd106c5738b23c58fb82d722
Kapil Garaniya <167281036+kapilgaraniya@users.noreply.github.com>
2024-06-27 11:05:42 +0530

    Add files via upload

---
+++ Practice/C++ Prectice/p_odd_even.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int n;
+	
+	cout<<"Enter Number : ";
+	cin>>n;
+	
+	if(n % 2 ==0)
+	{
+		cout<<n<<" Is Even Number...";
+	}
+	else
+	{
+		cout<<n<<" Is Odd Number...";
+	}
+}
