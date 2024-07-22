6c1cece1d4124b2fbc1a3a7ba8744d770bdec70f
Kapil Garaniya <167281036+kapilgaraniya@users.noreply.github.com>
2024-07-22 19:20:54 +0530

    Add files via upload

---
+++ Assignment/C++/Module-4 [C++]/11_swap_two_numbers.cpp
@@ -0,0 +1,9 @@
+// 11 Write a program to swap the two numbers using friend function
+
+#include<iostream>
+using namespace std;
+
+main()
+{
+	//write code..
+}
---
+++ Assignment/C++/Module-4 [C++]/14_delete_and_new_operator.cpp
@@ -0,0 +1,9 @@
+// 14. Create a example of use delete and new operator. 
+
+#include<iostream>
+using namespace std;
+
+main()
+{
+	//code...
+}
